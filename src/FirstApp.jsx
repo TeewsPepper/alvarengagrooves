128e62358d60c376f55376e00d2fdc72c9143881
TeewsPepper <gomez.pepper@gmail.com>
2024-07-07 15:07:26 -0300

    ajustes 2da revision

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,32 +1,37 @@
-import React from 'react';
+
+
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/layout/Layout";
-import Musica from "./views/Musica";
-import Portada from "./views/Portada";
-import Luthier from "./views/Luthier";
-import Register from "./views/Register";
-import Blog from "./views/Blog";
-import BlogPost from "./views/BlogPost";
-import Login from "./views/Login";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./routes/ProtectedRoute";
+
+const Layout = lazy(() => import("./components/layout/Layout"));
+const Musica = lazy(() => import("./views/Musica"));
+const Portada = lazy(() => import("./views/Portada"));
+const Luthier = lazy(() => import("./views/Luthier"));
+const Register = lazy(() => import("./views/Register"));
+const Blog = lazy(() => import("./views/Blog"));
+const BlogPost = lazy(() => import("./views/BlogPost"));
+const Login = lazy(() => import("./views/Login"));
 
 const FirstApp = () => {
   return (
     <>
       <AuthProvider>
         <Router>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Portada />} />
-              <Route path="musica" element={<Musica />} />
-              <Route path="luthier" element={<Luthier />} />
-              <Route path="login" element={<Login />} />
-              <Route path="register" element={<Register />} />
-              <Route path="blog" element={<ProtectedRoute> <Blog /></ProtectedRoute>}/>
-              <Route path="blog/:id"element={ <ProtectedRoute> <BlogPost /> </ProtectedRoute>} />
-            </Route>
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Portada />} />
+                <Route path="musica" element={<Musica />} />
+                <Route path="luthier" element={<Luthier />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="blog" element={<ProtectedRoute><Blog /></ProtectedRoute>} />
+                <Route path="blog/:id" element={<ProtectedRoute><BlogPost /></ProtectedRoute>} />
+              </Route>
+            </Routes>
+          </Suspense>
         </Router>
       </AuthProvider>
     </>
@@ -34,6 +39,7 @@ const FirstApp = () => {
 };
 
 export default FirstApp;
+
                 
                 
                   
